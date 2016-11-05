9f258961a18f6e3073606927a2f1002528f7887e
Mazdak <Mazdak.fatahi@gmail.com>
2016-11-05 16:31:12 +0330

    A new java class is added

--- app/src/main/java/com/example/sodco/mytestemtact/MainActivity.java
+++ app/src/main/java/com/example/sodco/mytestemtact/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //there is a tiny change
+
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/example/sodco/mytestemtact/TestClass.java
@@ -0,0 +1,10 @@
+package com.example.sodco.mytestemtact;
+
+/**
+ * Created by SODCO on 11/5/2016.
+ */
+
+public class TestClass {
+    public TestClass() {
+    }
+}
