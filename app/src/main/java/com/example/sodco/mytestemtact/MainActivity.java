3c5a2891d445ba2597a2e1e2aa1d17cefd685931
Mazdak <Mazdak.fatahi@gmail.com>
2016-11-05 16:20:32 +0330

    a comment has been added

--- app/src/main/java/com/example/sodco/mytestemtact/MainActivity.java
+++ app/src/main/java/com/example/sodco/mytestemtact/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //there is a tiny change
         setContentView(R.layout.activity_main);
     }
 }
